62c4a6639eee99d9b10e537cffc53010de990ad0
NathanHeang <110141@gltech.org>
2025-05-28 13:56:03 -0400

    add making box bigger when hovering over it idk what its called whatever

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 let cursor = document.querySelector("#cursor")
 let body = document.querySelector("body")
 
-document.addEventListener("mousemove", (e) => {
-    body.style.backgroundPositionX = e.pageX/-4 + "px";
-    body.style.backgroundPositionY = e.pageY/-4 + "px";
-    cursor.style.left = e.pageX + "px";
-    cursor.style.top = e.pageY + "px";
+document.addEventListener("mousemove", (event) => {
+    body.style.backgroundPositionX = event.pageX/-4 + "px";
+    body.style.backgroundPositionY = event.pageY/-4 + "px";
+    cursor.style.left = event.clientX + "px";
+    cursor.style.top = event.clientY + "px";
 })
