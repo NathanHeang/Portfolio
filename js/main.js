6ef5572db0d023bc97ec8e54f67c8b9b37f1d799
NathanHeang <110141@gltech.org>
2025-06-02 13:58:41 -0400

    cleanup stuff and fix hiding boxes

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ let cursor = document.querySelector("#cursor")
 let body = document.querySelector("body")
 
 document.addEventListener("mousemove", (event) => {
-    body.style.backgroundPositionX = event.pageX/-4 + "px";
-    body.style.backgroundPositionY = event.pageY/-4 + "px";
     cursor.style.left = event.clientX + "px";
     cursor.style.top = event.clientY + "px";
 })
