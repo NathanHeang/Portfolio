48e5971b8546d787986fb93b8ee74c6358794fbe
NathanHeang <110141@gltech.org>
2025-06-10 13:55:32 -0400

    moooore animati9onnnndndfndfgn

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 let cursor = document.querySelector("#cursor")
 let body = document.querySelector("body")
+let lastUpdated = document.querySelector("#lastUpdated")
+
+/*lastUpdated.textContent = "// Last Updated " + document.lastModified + " //"*/
 
 document.addEventListener("mousemove", (event) => {
     cursor.style.left = event.clientX + "px";
